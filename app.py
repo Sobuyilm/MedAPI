67ab76040e9d5e28b98437c0889e752082be9379
Sobuyilm <82869448+Sobuyilm@users.noreply.github.com>
2021-06-30 04:09:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 #Import stuff
-from flask import Flask
 import pytesseract as tess
 from PIL import Image
-import spacy
 import requests
 from flask import Flask, request, jsonify
 
-med7 = spacy.load("en_core_med7_lg")
+
 app = Flask(__name__)
 
 @app.route("/", methods=["POST"])
@@ -17,14 +15,11 @@ def process_image():
     img = Image.open(file.stream)
     text = tess.image_to_string(img)
 
-    #Break down text into array
-    doc = med7(text)
-    spacy.displacy.render(doc, style='ent')
-
-    #Extract drug from array
-    analysis = [(ent.text, ent.label_) for ent in doc.ents]
-    drug = analysis[0]
-    Medizin = drug[0]
+    #Break down text into array & Extract drug from array
+    for i in text.split():
+        midresponse = requests.get("https://www.drugs.com/" + i)
+        if midresponse.ok:
+            Medizin = i
 
     #Dosage&Administration
     response = requests.get("https://api.fda.gov/drug/label.json?search=dosage_and_administration:" + Medizin)
