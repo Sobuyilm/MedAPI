a7a317815f54d92d8aca85e8fdfabd4aacdfc356
Sobuyilm <82869448+Sobuyilm@users.noreply.github.com>
2021-07-11 20:12:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 from flask import Flask, request, jsonify
 from PIL import Image
 import pytesseract
+import requests
 pytesseract.pytesseract.tesseract_cmd = './.apt/usr/bin/tesseract'
 
 app = Flask(__name__)
 text = "b"
 @app.route("/imgt", methods=["POST", "GET"])
-
-
 def process_image():
     global text
     if request.method == 'POST':
         file = request.files['image']
         img = Image.open(file.stream)
         text = pytesseract.image_to_string(img, lang='eng')
- 
-        return jsonify(text)
+
+        for i in text.split():
+            midresponse = requests.get("https://www.drugs.com/" + i)
+            if midresponse.ok:
+                Medizin = str(i)
+                response = requests.get("https://api.fda.gov/drug/label.json?search=dosage_and_administration:" + Medizin)
+
+        for data in (response.json()["results"]):
+            DosageAndAdministration = data["dosage_and_administration"]
+            return jsonify(DosageAndAdministration)
+
+        
 
 @app.route('/')
 def index():
